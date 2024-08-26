85ee20481ab0ce34dd0a9cd9d0cba27722544113
MashiGitCat <maheshinawarathna32@gmail.com>
2024-08-25 17:31:44 -0700

    create hero section and add carousel

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../../styles/main.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "./main.css";
 
 const Main = () => {
   return (
@@ -20,12 +21,64 @@ const Main = () => {
           </section>
         </nav>
       </header>
-      <main>
+      <main className= "main">
         <section className="main__hero">
-          <section className="main__hero__container">
-            <section className="main__hero__images"></section>
-          </section>
-      </section>
+          <div className="card  main__hero__text-container">
+            <div className="card-body">
+              <h1 className="card-title main__hero__main-header-style">
+                KittyConnect <br></br> to Connect Kitties With Owners.
+              </h1>
+              <h4 className="card-text main__hero__sub-header-style">
+                Where Every Meow Leads to a Forever Home.
+              </h4>
+              <div className="main__hero__button-container">
+                <div className="d-grid gap-2">
+                  <button className="btn btn-dark" type="button">
+                    Find Cats
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card main__hero__image-container">
+            <div id="heroCarousel" className="carousel slide" data-bs-ride="carousel">
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <div
+                    className="main__hero__images"
+                    style={{
+                      backgroundImage: `url("https://res.cloudinary.com/maheshidevelopments/image/upload/c_fill,w_320,h_320,ar_1:1/v1724312738/kittyconnect-heroimage1.jpg")`,
+                    }}
+                  ></div>
+                </div>
+                <div className="carousel-item">
+                  <div
+                    className="main__hero__images"
+                    style={{
+                      backgroundImage: `url("https://res.cloudinary.com/maheshidevelopments/image/upload/c_fill,w_320,h_320,ar_1:1/v1724631814/kitty-connect-image2.jpg")`,
+                    }}
+                  ></div>
+                </div>
+                <div className="carousel-item">
+                  <div
+                    className="main__hero__images"
+                    style={{
+                      backgroundImage: `url("https://res.cloudinary.com/maheshidevelopments/image/upload/c_fill,w_320,h_320,ar_1:1/v1724631980/kitty-connect-image3.jpg")`,
+                    }}
+                  ></div>
+                </div>
+              </div>
+              <button className="carousel-control-prev" type="button" data-bs-target="#heroCarousel" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button className="carousel-control-next" type="button" data-bs-target="#heroCarousel" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
